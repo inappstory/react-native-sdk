2774a1469140659d08c0a9ec178c4312db91be1e
a.strebizh <artem.strebizh@inappstory.com>
2024-05-26 22:30:28 +0300

    feat: update RN to 0.74.1

--- examples/RnIasExample/metro.config.js
+++ examples/RnIasExample/metro.config.js
@@ -1,62 +1,11 @@
-/* eslint-disable import/no-commonjs */
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const path = require("path");
-const exclusionList = require("metro-config/src/defaults/exclusionList");
-const escape = require("escape-string-regexp");
-const pack = require("../../npm/react-native-ias/package.json");
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
- * @type {import("metro-config").MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-
-
-
-// react-native-ias root directory
-const rniasRoot = path.resolve(__dirname, "../../npm/react-native-ias/");
-
-const modules = [
-    "@react-navigation/native",
-    "react-native-safe-area-context",
-    "react-native-gesture-handler",
-    "react-native-reanimated",
-    ...Object.keys(pack.peerDependencies),
-];
-
-const nodeModulesPaths = [path.resolve(path.join(__dirname, './node_modules'))];
-
-
-const config = {
-    projectRoot: __dirname,
-    watchFolders: [rniasRoot],
-
-    // We need to make sure that only one version is loaded for peerDependencies
-    // So we exclude them at the root, and alias them to the versions in example's node_modules
-    resolver: {
-        unstable_enableSymlinks: true,
-        blockList: exclusionList(
-            modules.map(
-                m =>
-                    new RegExp(`^${escape(path.join(rniasRoot, "node_modules", m))}\\/.*$`),
-            ),
-        ),
-
-        extraNodeModules: modules.reduce((acc, name) => {
-            acc[name] = path.join(__dirname, "node_modules", name);
-            return acc;
-        }, {}),
-        nodeModulesPaths,
-    },
-
-    transformer: {
-        getTransformOptions: async () => ({
-            transform: {
-                experimentalImportSupport: true,
-                inlineRequires: true,
-            },
-        }),
-    },
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
